3bdb072547600c7f1b99345c3e0608705362643e
A-Yeong <dkdud064@kumoh.ac.kr>
2024-05-30 16:02:32 +0900

    초기 커밋.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OTTProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
